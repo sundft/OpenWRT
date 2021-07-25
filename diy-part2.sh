13704bd1e7285c40f9c1c960a7fa842c3db17b59
sundft <58099789+sundft@users.noreply.github.com>
2021-07-25 15:22:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,5 +37,5 @@ echo \"dhcp-option=15\" >> /etc/dnsmasq.conf\
 echo \"dhcp-option=28\" >> /etc/dnsmasq.conf' package/lean/default-settings/files/zzz-default-settings
 
 # Replace theme
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
