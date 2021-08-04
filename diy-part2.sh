8e7d59637cb8be3f080c228e7bc5f8de18203c66
sundft <58099789+sundft@users.noreply.github.com>
2021-08-04 14:35:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,13 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default PW
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$lDRVkTLs$EUprnJdwldZBax.6Osjxo0:18190:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
-# Modify iptables set
-#sed -i 's/echo \"iptables/echo \"# iptables/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/echo '"'"'iptables/echo '"'"'# iptables/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/echo '"'"'\[ -n/echo '"'"'# \[ -n/g' package/lean/default-settings/files/zzz-default-settings
 
-# Set default LAN/WAN
 sed -i '/uci commit fstab/a\
 \
 uci set network.lan.ipaddr=\"192.168.16.1\"\
@@ -28,14 +26,9 @@ uci set network.wan.ifname=\"eth1\"\
 uci set network.wan6.ifname=\"eth1\"\
 uci commit network' package/lean/default-settings/files/zzz-default-settings
 
-# IPTV
 sed -i '/log-facility=\/dev\/null/a\
 echo \"\" >> /etc/dnsmasq.conf\
 echo \"# IPTV\" >> /etc/dnsmasq.conf\
 echo \"dhcp-option-force=125,00:00:00:00:14:02:06:48:47:57:2D:43:54:0A:02:20:00:0B:02:00:55:0D:02:00:2E\" >> /etc/dnsmasq.conf\
 echo \"dhcp-option=15\" >> /etc/dnsmasq.conf\
 echo \"dhcp-option=28\" >> /etc/dnsmasq.conf' package/lean/default-settings/files/zzz-default-settings
-
-# Replace theme
-#rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
